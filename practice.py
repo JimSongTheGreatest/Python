a5b930d4357afcf779d29dfdd9ede1e201e4b0e3
Ji Min Song <sjm9509@gmail.com>
2020-06-29 03:13:30 +0900

    update

--- practice.py
+++ practice.py
@@ -1,3 +1,33 @@
-print(5)
-print(-10)
-print(3.14)
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# print('ballon')
+# print("balloon")
+# print("hahahahahahaha")
+# print("ha"*9)
+# print("ha"*9)
+
+# # 참 / 거짖
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not (not 5 > 10))
+
+# # 애완동물을 소개해 주세요~
+# animal = "강아지"
+# name = "연탄이"
+# age = 4
+# hobby = "산책"
+# is_adult = age >= 3
+
+# print("우리집 " + animal + "의 이름은 " + name + "예요")
+# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
